bd35058e23e701e2358b0b9e397aa3f60423354f
Ryan Li <conbas2019@gmail.com>
2021-07-15 23:50:53 +0800

    feat: return error if subcommand missing

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
 use buz::percent::percent_encode;
 use clap::{load_yaml, App};
-use std::io::{self, Write};
 use std::str;
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
-    let mut app = App::from(yaml);
+    let app = App::from(yaml);
     let matches = app.clone().get_matches();
 
-    let output = match matches.subcommand() {
+    match matches.subcommand() {
         Some(("percent", sub_m)) => percent_encode(sub_m),
-        _ => help_info(&mut app),
+        _ => eprintln!("{}: SUBCOMMAND is required", env!("CARGO_PKG_NAME")),
     };
-    let mut out = io::stdout();
-    out.write_all(&output.as_bytes())
-        .expect("unable writing into stdout");
 }
 
-fn help_info(app: &mut App) -> String {
-    let mut help = Vec::new();
-    app.write_help(&mut help).expect("unable to write help");
-    str::from_utf8(&help)
-        .expect("unable converting help into string")
-        .to_owned()
-}
+// fn help_info(app: &mut App) -> String {
+//     let mut help = Vec::new();
+//     app.write_help(&mut help).expect("unable to write help");
+//     str::from_utf8(&help)
+//         .expect("unable converting help into string")
+//         .to_owned()
+// }
--- src/percent.rs
+++ src/percent.rs
@@ -3,10 +3,10 @@ use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 
 use std::{
     fs,
-    io::{self, Read},
+    io::{self, Read, Write},
 };
 
-pub fn percent_encode(matches: &ArgMatches) -> String {
+pub fn percent_encode(matches: &ArgMatches) {
     let input = match matches.value_of("input") {
         Some(path) => fs::read_to_string(path).expect("cannot read input file"),
         None => {
@@ -17,5 +17,8 @@ pub fn percent_encode(matches: &ArgMatches) -> String {
             buffer
         }
     };
-    utf8_percent_encode(&input, NON_ALPHANUMERIC).to_string()
+    let output = utf8_percent_encode(&input, NON_ALPHANUMERIC).to_string();
+    let mut out = io::stdout();
+    out.write_all(&output.as_bytes())
+        .expect("unable writing into stdout");
 }
