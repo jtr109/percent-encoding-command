8b48d1552ebcadd017435d1d0682c9de52ed185a
Ryan Li <conbas2019@gmail.com>
2021-07-16 17:56:29 +0800

    feat: add tests for core functions

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,28 @@ pub fn decode(reader: &mut dyn Read, writer: &mut dyn Write) -> Result<()> {
     writer.write(decoded.as_bytes())?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn encode_works() {
+        let mut reader = "hello world 你好".as_bytes();
+        let mut writer = vec![];
+        encode(&mut reader, &mut writer).expect("unexpected encode error");
+        let result = std::str::from_utf8(&writer).unwrap();
+        let expected = "hello%20world%20%E4%BD%A0%E5%A5%BD";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn decode_works() {
+        let mut reader = "hello%20world%20%E4%BD%A0%E5%A5%BD".as_bytes();
+        let mut writer = vec![];
+        decode(&mut reader, &mut writer).expect("unexpected decode error");
+        let result = std::str::from_utf8(&writer).unwrap();
+        let expected = "hello world 你好";
+        assert_eq!(result, expected);
+    }
+}
