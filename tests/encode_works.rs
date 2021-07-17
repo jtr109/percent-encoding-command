0836dcde2015d6659cc53f5e79fe299fd4ea9fdd
Ryan Li <conbas2019@gmail.com>
2021-07-17 10:21:23 +0800

    feat: add test about input file not exists condition

--- tests/encode_works.rs
+++ tests/encode_works.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
 use assert_cmd::Command;
-use tempfile::NamedTempFile;
+use tempfile::{NamedTempFile, TempDir};
 
 const PLAIN_TEXT: &str = "hello world 你好";
 const EXPECTED_ENCODED: &str = "hello%20world%20%E4%BD%A0%E5%A5%BD";
 const COMMAND: &str = "pct";
 
 #[test]
-fn encode_works_with_piped_input() -> Result<()> {
+fn success_encode_with_piped_input() -> Result<()> {
     let temp_file = NamedTempFile::new()?;
     std::fs::write(temp_file.path(), PLAIN_TEXT)?;
     let mut cmd = Command::cargo_bin(COMMAND)?;
@@ -17,7 +17,7 @@ fn encode_works_with_piped_input() -> Result<()> {
 }
 
 #[test]
-fn encode_works_with_stdin() -> Result<()> {
+fn success_encode_with_stdin() -> Result<()> {
     let mut cmd = Command::cargo_bin(COMMAND)?;
     let assert = cmd.write_stdin(PLAIN_TEXT).assert();
     assert.success().stdout(EXPECTED_ENCODED);
@@ -25,7 +25,7 @@ fn encode_works_with_stdin() -> Result<()> {
 }
 
 #[test]
-fn encode_works_with_input_file() -> Result<()> {
+fn success_encode_with_input_file() -> Result<()> {
     let tmp_file = NamedTempFile::new()?;
     std::fs::write(tmp_file.path(), PLAIN_TEXT)?;
     let mut cmd = Command::cargo_bin(COMMAND)?;
@@ -34,7 +34,17 @@ fn encode_works_with_input_file() -> Result<()> {
     Ok(())
 }
 
-// accept input flag
+#[test]
+fn failure_encode_with_file_does_not_exist() -> Result<()> {
+    let tmp_dir = TempDir::new()?;
+    let file_path = tmp_dir.into_path().join("not-exists.txt");
+    let mut cmd = Command::cargo_bin(COMMAND)?;
+    let assert = cmd.arg("--input").arg(file_path).assert();
+    assert.failure();
+    Ok(())
+}
+
+// error if input file not exists
 // accept output flag
 // relative input path
 // relative output path
