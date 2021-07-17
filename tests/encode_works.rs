46b96b3c45084823bc955503405cfe4697643081
Ryan Li <conbas2019@gmail.com>
2021-07-17 09:53:13 +0800

    feat: use tmp file as stdin

--- tests/encode_works.rs
+++ tests/encode_works.rs
@@ -1,22 +1,24 @@
 use anyhow::Result;
 use assert_cmd::Command;
+use tempfile::NamedTempFile;
 
 const PLAIN_TEXT: &str = "hello world 你好";
 const EXPECTED_ENCODED: &str = "hello%20world%20%E4%BD%A0%E5%A5%BD";
+const COMMAND: &str = "pct";
 
 #[test]
 fn encode_works_with_piped_input() -> Result<()> {
-    let mut cmd = Command::cargo_bin("pct")?;
-    let assert = cmd
-        .pipe_stdin(std::path::Path::new("./tests/fixtures/hello.txt"))?
-        .assert();
+    let temp_file = NamedTempFile::new()?;
+    std::fs::write(temp_file.path(), PLAIN_TEXT)?;
+    let mut cmd = Command::cargo_bin(COMMAND)?;
+    let assert = cmd.pipe_stdin(temp_file.path())?.assert();
     assert.success().stdout(EXPECTED_ENCODED);
     Ok(())
 }
 
 #[test]
 fn encode_works_with_stdin() -> Result<()> {
-    let mut cmd = Command::cargo_bin("pct")?;
+    let mut cmd = Command::cargo_bin(COMMAND)?;
     let assert = cmd.write_stdin(PLAIN_TEXT).assert();
     assert.success().stdout(EXPECTED_ENCODED);
     Ok(())
