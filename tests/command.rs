827770bc51411d86b3d9e244e8079558121acee1
Ryan Li <conbas2019@gmail.com>
2021-07-17 12:51:53 +0800

    fix: not change current directory

--- tests/command.rs
+++ tests/command.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 use assert_cmd::Command;
+use pathdiff::diff_paths;
+use predicates::prelude::predicate;
 use tempfile::{NamedTempFile, TempDir};
 
 const PLAIN_TEXT: &str = "hello world 你好";
@@ -40,20 +42,20 @@ fn failure_encode_with_file_does_not_exist() -> Result<()> {
     let file_path = tmp_dir.into_path().join("not-exists.txt");
     let mut cmd = Command::cargo_bin(COMMAND)?;
     let assert = cmd.arg("--input").arg(file_path).assert();
-    assert.failure();
+    assert
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
     Ok(())
 }
 
 #[test]
 fn success_encode_with_relative_input() -> Result<()> {
     let tmp_file = NamedTempFile::new()?;
-    std::env::set_current_dir(tmp_file.path().parent().expect("tmp dir not exists"))?;
-    std::fs::write(
-        tmp_file.path().file_name().expect("unable to get filename"),
-        PLAIN_TEXT,
-    )?;
+    let current_dir = std::env::current_dir().unwrap();
+    let relative_path = diff_paths(&tmp_file.path(), current_dir).unwrap();
+    std::fs::write(relative_path.clone(), PLAIN_TEXT)?;
     let mut cmd = Command::cargo_bin(COMMAND)?;
-    let assert = cmd.arg("--input").arg(tmp_file.path()).assert();
+    let assert = cmd.arg("--input").arg(relative_path).assert();
     assert.success().stdout(ENCODED);
     Ok(())
 }
@@ -90,20 +92,35 @@ fn success_encode_with_output_when_file_exists() -> Result<()> {
 }
 
 #[test]
-fn success_encode_with_relative_output_path() -> Result<()> {
-    let tmp_dir = TempDir::new()?;
-    std::env::set_current_dir(tmp_dir.path())?;
-    let relative_path = "test.txt";
-    let mut cmd = Command::cargo_bin(COMMAND)?;
+fn success_encode_with_relative_output_path() {
+    let tmp_file = NamedTempFile::new().unwrap();
+    let current_dir = std::env::current_dir().unwrap();
+    let relative_path = diff_paths(&tmp_file.path(), current_dir).unwrap();
+    let mut cmd = Command::cargo_bin(COMMAND).unwrap();
     let assert = cmd
         .write_stdin(PLAIN_TEXT)
         .arg("--output")
         .arg(relative_path)
         .assert();
     assert.success();
-    let content = std::fs::read_to_string(tmp_dir.path().join(relative_path))?;
+    let content = std::fs::read_to_string(tmp_file.path()).unwrap();
     assert_eq!(content, ENCODED);
-    Ok(())
 }
 
-// if directory not exists
+#[test]
+fn error_if_dir_of_output_not_exists() -> Result<()> {
+    let tmp_file_path = {
+        let tmp_dir = TempDir::new()?;
+        tmp_dir.path().join("test.txt")
+    };
+    let mut cmd = Command::cargo_bin(COMMAND)?;
+    let assert = cmd
+        .write_stdin(PLAIN_TEXT)
+        .arg("--output")
+        .arg(tmp_file_path.clone())
+        .assert();
+    assert
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
+    Ok(())
+}
